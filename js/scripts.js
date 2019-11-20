7a9760b9553ce028c629c83a7d588d426196a1af
xkored <xkored@gmail.com>
2019-11-20 13:11:55 +0400

    feat: click on checkbox

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,9 @@ $(function() {
     navText: ["<div class='d-flex align-items-center'><svg width='11' height='18' viewBox='0 0 11 18' xmlns='http://www.w3.org/2000/svg'><path d='M10 1L2 9L10 17' stroke-width='2'/></svg><span class='ml-2'>Назад</span></div>","<div class='d-flex align-items-center'><span class='mr-2'>Вперед</span><svg width='11' height='18' viewBox='0 0 11 18' xmlns='http://www.w3.org/2000/svg'><path d='M1 17L9 9L1 0.999999' stroke-width='2'/></svg></div>"],
     items: 1,
   });
+  $('.popup_quiz__bot .checkbox').on('click', function() {
+    $('.popup_quiz__bot .owl-next').trigger('click');
+  })
   $('[data-fancybox]').fancybox({
     animationDuration : 600,
     animationEffect   : 'slide-in-out',
